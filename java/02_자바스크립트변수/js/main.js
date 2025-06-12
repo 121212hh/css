298a780e6b63b6472e74107edb42a199f33b239c
121212hh <qwaszx12368@naver.com>
2025-06-12 14:41:52 +0900

    Add files via upload

---
+++ java/02_자바스크립트변수/js/main.js
@@ -0,0 +1,24 @@
+let kor = 100;
+let name = "김포장기"
+
+kor = 200;
+
+document.write(kor + "<br>");
+document.write(name + "<br>");
+
+alert(name);
+alert(kor);
+
+
+// let irm = "컴퓨터학원";
+// let age = 30;
+// let address = "경기도 김포시 장기동";
+let irm = "컴퓨터학원",age = "30",address = "장기동"
+document.write("<br>이름=" + irm + "<br>");
+document.write("나이=" + age + "<br>");
+document.write("주소=" + address + "<br>");
+
+
+
+const pi = 3.14;
+document.write("파이변수에 기억된 값=" + pi );
