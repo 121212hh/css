5bbecca40e4ed3ea3e6117ed77dc8bd93913a329
121212hh <qwaszx12368@naver.com>
2025-06-17 14:48:00 +0900

    Add files via upload

---
+++ 2_selectcss/js/main.js
@@ -0,0 +1,18 @@
+// 제이쿼리 준비작업 (바인딩)
+$(function(){
+    // 제이쿼리 문법 삽입
+    // $("선택자").명령함수()
+    // 명령함수 css()
+    $("h1").css({"color":"red"});
+    $(".data").css({"font-size":"80px","color":"pink","background":"purple"});
+    $(".para").css({"font-size":"10px","border":"10px solid blue"});
+    $("ul li:last-child").css({"font-size":"48px"});
+    $("a").css({"padding":"10px","border":"5px solid blue"});
+
+
+
+
+
+
+
+})
