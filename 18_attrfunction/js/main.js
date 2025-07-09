dec921c55f8eed6889311f1b244b98bbec380216
121212hh <qwaszx12368@naver.com>
2025-07-09 17:21:26 +0900

    Add files via upload

---
+++ 18_attrfunction/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".box ul li").mouseenter(function(){
+        let num = $(this).index()
+        console.log(num)
+
+        let url = $(this).find('img').attr('src')
+        console.log(url)
+
+        $(".box .big img").attr({'src':url})
+    })
+})
