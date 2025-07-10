8936fd9c05427dd1e608fa15e715386ad04009c1
121212hh <qwaszx12368@naver.com>
2025-07-10 17:11:08 +0900

    Add files via upload

--- 08_m_slide_subway/js/main.js
+++ 08_m_slide_subway/js/main.js
@@ -24,4 +24,28 @@ $(function(){
     el: '.swiper-scrollbar',
   },
 });
+
+$('.banner_slide').slick({
+  autoplay: true,
+  slidesToShow:2,
+  arrows: false,
+  dots: true,
+});
+// 모바일메뉴
+$(".m_gnb>ul>li>a").click(function(){
+  $(this).next().slideToggle().parent().siblings().find(".depth").slideUp();
+})
+
+
+$(".header .m_btn").click(function(){
+      $(".cover").fadeIn()
+  $(".m_slidewrap").animate({"left":0},300)
+  })
+  
+})
+
+
+$(".m_close").click(function(){
+    $(".cover").fadeOut()
+  $(".m_slidewrap").animate({"left":'-100%'},300)
 })
