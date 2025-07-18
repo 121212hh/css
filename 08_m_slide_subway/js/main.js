b647fb41532b4667196361b97df10e0c28e4ce81
121212hh <qwaszx12368@naver.com>
2025-07-18 15:20:07 +0900

    Add files via upload

--- 08_m_slide_subway/js/main.js
+++ 08_m_slide_subway/js/main.js
@@ -78,4 +78,46 @@ $("ul.ri_icon li").click(function(){
   $("ul.ri_icon span").removeClass('on')
 
   $(this).addClass('on')
+    // 쿠키정보확인
+    if(GetCookie)
+  // 팝업창
+  $('#today_close,.popup .close label').click(function(){
+    // setCookie
+    if($('#today_close').is(':checked')){
+      setCookie('subway','today',1)
+      $('.popup').hide()
+    }else{
+      $('.popup').hide()
+    }
+    // 닫기버튼팝업숨기기
+    $('.popup .close .txt_btn').click(function(){
+      $('.popup').hide()
+    })
+
+  })
+  // // GetCookie
+	// 		function GetCookie(name){
+	// 			var value=null, search=name+"=";
+	// 			if(document.cookie.length > 0){
+	// 				var offset=document.cookie.indexOf(search);
+	// 				if(offset != -1){
+	// 					offset+=search.length;
+	// 					var end=document.cookie.indexOf(";", offset);
+	// 					if(end == -1) end=document.cookie.length;
+	// 					value=unescape(document.cookie.substring(offset, end));
+	// 				}
+	// 			} return value;
+	// 		}
+	// 		// SetCookie
+	// 		function setCookie(name, value, expiredays){
+	// 			var days=10;
+	// 			if(days){
+	// 				var date=new Date(); 
+	// 				date.setTime(date.getTime()+(days*24*60*60*1000));
+	// 				var expires="; expires="+date.toGMTString();
+	// 			}else{
+	// 				var expires=""; 
+	// 			}
+	// 			document.cookie=name+"="+value+expires+"; path=/";
+	// 		}
 })
